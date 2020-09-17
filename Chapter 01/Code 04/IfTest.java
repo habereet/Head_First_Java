51842463d2218589111041da6f2f13efbf38ae7a
habereet <habereet@gmail.com>
2020-09-16 22:23:34 -0400

    Adding Fourth block of code

---
+++ Chapter 01/Code 04/IfTest.java
@@ -0,0 +1,14 @@
+/* Page 13 in the book
+   Page 47 in the PDF
+   Using an if loop
+*/
+
+public class IfTest {
+	public static void main (String[] args) {
+		int x = 3;
+		if (x == 3) {
+			System.out.println("x must be 3");
+		}
+		System.out.println("This runs no matter what.");
+	}
+}
---
+++ Chapter 01/Code 04/README.md
@@ -0,0 +1,7 @@
+Page 13 in the book
+
+Page 47 in the PDF
+
+*Using an if test test for the first time in the book*
+
+No else condition
