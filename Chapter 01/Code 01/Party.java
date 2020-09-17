d184a708bf3093586bf65195857231741f6846f5
habereet <habereet@gmail.com>
2020-09-16 21:31:53 -0400

    spelling in comment - trivial

--- Chapter 01/Code 01/Party.java
+++ Chapter 01/Code 01/Party.java
@@ -3,7 +3,7 @@ import java.awt.event.*;
 
 /* Page 3 in the book
    Page 37 in the PDF
-   This code is ONLY an exampl and won't work
+   This code is ONLY an example and won't work
 */
 
 class Party	{
