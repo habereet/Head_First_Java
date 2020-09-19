236e0a434262681131f977ef19f4d849ab4b83f1
habereet <habereet@gmail.com>
2020-09-19 16:49:00 -0400

    Code for worksheet

---
+++ Chapter_01/07_Worksheet_02/DooBee.java
@@ -0,0 +1,15 @@
+// Code from worksheet with my answers
+
+public class DooBee {
+	public static void main(String[] args) {
+		int x = 1;
+		while (x < 3) { // 3 <-- We want it to do this loop twice
+			System.out.print("Doo"); // print <- Will not add a new line
+			System.out.print("Bee"); // print <- Will not add a new line
+			x = x + 1;
+		}
+		if (x == 3) { // 3 <-- exited while loop @3
+			System.out.print("Do");
+		}
+	}
+}
